64ca8cbeac62843fc19c3cc81805a0aa96f27ca0
adibbz2004 <rafiqoadib1@gmail.com>
2025-03-05 10:40:11 +0700

    Praktikum2.5_Jobsheet4_20

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,24 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash; // Tambahkan untuk Hash::make()
-
-use function Laravel\Prompts\password;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager44', 
-                'nama' => 'Manager Empat Empat',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-            ]
-        );
+        $user->nama = 'Manager11';
+        $user->password = Hash::make('12345');
+        $user->level_id = 2;
+
+        $user->username = 'manager12';
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
