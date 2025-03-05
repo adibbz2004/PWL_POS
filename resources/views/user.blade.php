2c697343b992d1e92e6c7a8fb49898646cca1fad
adibbz2004 <rafiqoadib1@gmail.com>
2025-03-05 14:30:25 +0700

    Praktikum2.6_Jobsheet4_20

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,47 +1,25 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User</title>
-    <style>
-        body {
-            font-family: "Times New Roman", serif;
-            margin: 20px;
-            padding: 20px;
-            text-align: center;
-        }
-        table {
-            width: 50%;
-            margin: 0 auto;
-            border-collapse: collapse;
-            font-size: 16px;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
 <body>
     <h1>Data User</h1>
+    <a href="{{url('/user/tambah')}}">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>
+                <a href="{{url('/user/ubah/'. $d->user_id) }}">Ubah</a> | 
+                <a href="{{url('/user/hapus/' . $d->user_id) }}">Hapus</a>
+            </td>
         </tr>
+        @endforeach
     </table>
 </body>
-</html>
